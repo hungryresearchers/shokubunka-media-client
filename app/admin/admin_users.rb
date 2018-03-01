3293b51d5a4eff4b6817a4340b4aa688a0214dc4
yasuno0327 <5315076@ed.tus.ac.jp>
2018-03-01 09:42:35 +0900

    erb to slim

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register AdminUser do
   permit_params :email, :password, :password_confirmation
 
+  actions :all, except: [:create, :destroy]
+
   index do
     selectable_column
     id_column
@@ -15,15 +17,5 @@ ActiveAdmin.register AdminUser do
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
-
-  form do |f|
-    f.inputs do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-    end
-    f.act
-    ions
-  end
 
 end

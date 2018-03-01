37ad8934f84c0bae08f19b65c2124be5d850af00
yasuno0327 <5315076@ed.tus.ac.jp>
2018-03-02 01:32:44 +0900

    Add devise view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :writers, only: [:sign_in, :sign_out, :session]
   ActiveAdmin.routes(self)
 end

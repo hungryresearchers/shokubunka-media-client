3b5b9ea0479b7516fe0c44512af1d187a647842d
hiroki <623dgk511522@gmail.com>
2018-03-24 17:50:11 +0900

    Post CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :writers, only: [:sign_in, :sign_out, :session]
   ActiveAdmin.routes(self)
+  root to: 'posts#index'
+
+  resources :posts
 end

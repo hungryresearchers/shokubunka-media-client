7c443eb7ee5ecd9a4c4edf89ef405d2b06bde05d
hiroki <623dgk511522@gmail.com>
2018-03-27 14:45:29 +0900

    Fixed

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: ckeditor_assets
+#
+#  id                :integer          not null, primary key
+#  data_file_name    :string(255)      not null
+#  data_content_type :string(255)
+#  data_file_size    :integer
+#  type              :string(30)
+#  width             :integer
+#  height            :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
 

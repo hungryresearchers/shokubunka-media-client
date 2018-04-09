7e7f80446cf8ef0b6354c7d65ecd533b6f54dfa6
Ryuichi Sakagami <shagamiiiii@gmail.com>
2018-04-09 14:01:13 +0900

    :sparkles: add eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'standard',
+    'plugin:react/recommended',
+  ],
+  plugins: [
+    'flowtype',
+    'react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-multi-spaces': 'off',
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'brace-style': ['error', 'stroustrup'],
+    'react/display-name': 'off',
+    'react/jsx-boolean-value': 'error',
+    'react/no-redundant-should-component-update': 'error',
+    'react/no-typos': 'error',
+    'react/no-unused-state': 'error',
+    'react/no-unused-prop-types': 'error',
+    'flowtype/define-flow-type': 1,
+    'flowtype/use-flow-type': 1,
+  },
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// @flow
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import './index.css'
+// import App from './App'
+// import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<div>保gへh</div>, document.getElementById('root'))
+// registerServiceWorker()
